ad539847730a28da71abe89e185cdc5022b209ef
Nivesh Padamata <2400030538@kluniversity.in>
2026-04-09 14:35:09 +0530

    Delete backend directory

--- backend/src/main/java/com/reviewin/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.reviewin.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
